9590bb2c5b0faecd25ad3de2095ee7866b2594bb
Lily <lily@lili.moe>
2025-05-25 22:43:53 +0300

    astro configuration, sitemap, etc.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
+
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://lili.moe',
+  base: '/',
+  server: {host: true, open: "/"},
+  trailingSlash: "ignore",
+  compressHTML: true,
+  integrations: [sitemap()]
+});
